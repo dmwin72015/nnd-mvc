e7bacb0e8a6ed69ca45e1514f490eef1da531085
小肉马马 <mjj@xiaomamadeMacBook-Pro.local>
2017-08-16 18:25:56 +0800

    重构

--- routes/admin/login.js
+++
@@ -1,9 +0,0 @@
-let user = require('../../controllers/user');
-
-module.exports = {
-    '/': function (req, res, next) {
-        res.render('login', {
-            titleName: '登录'
-        });
-    }
-};
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ module.exports = {
         if (req.originalUrl == '/favicon.ico') {
             return;
         }
-        res.render('login', {titleName: '登录页'});
+        if(req.session.loginInfo){
+            res.redirect('/admin/article');
+        }else{
+            res.render('login', {titleName: '登录页'});
+        }
     },
     '/login': function (req, res, next) {
         res.render('login', {titleName: '登录页'});
